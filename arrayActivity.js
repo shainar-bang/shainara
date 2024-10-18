a0f7c39f7b45d0e39a2470b7adb7a11ba491b2f0
shainar-bang <rabangshaina@gmail.com>
2024-10-18 18:37:20 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
++ // Favorite Singers
++ let favSingers = ["Olivia Rodrigo, Billie Eilish"];
++ console.log("My first favorite singer is", favSingers[0]);
++
++ // Favorite Numbers
++ let favNumbers = ["4", "19", "12", "2", "25"];
++ let mixedArr = ["string", ["otherarray"], 123, true];
++ console.log(mixedArr);
